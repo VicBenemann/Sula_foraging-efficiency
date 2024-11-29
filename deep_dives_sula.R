30ff5282c18734b216f9f40ce9da53b411a326c8
Bianca Costa Ribeiro <121443306+biancrib@users.noreply.github.com>
2024-11-29 12:13:41 -0300

    Update deep_dives_sula.R
    Adicionei as métricas de estatística básica para os mergulhos de SPSP e MS

--- deep_dives_sula.R
+++ deep_dives_sula.R
@@ -37,8 +37,6 @@ if ("Pressure" %in% colnames(data_SP)) {
 head(data_MS$Depth)
 head(data_SP$Depth)
 
-# Agora vamos adicionar uma coluna "Source" em cada uma das planilhas pra identificar cada ilha no gráfico
-
 # Adicionar a coluna de origem (SP ou MS)
 data_MS <- data_MS %>%
   mutate(Source = "MS")  # Adiciona a coluna "Source" com valor "MS"
@@ -96,58 +94,6 @@ plot_SP
 combined_plot <- plot_MS + plot_SP
 print(combined_plot)
 
-#-----CALCULO ODBA e VeDBA--------
-
-# Função para calcular ODBA e VeDBA para cada indivíduo
-calcular_vedba_odba <- function(df) {
-  mean_x <- mean(df$X, na.rm = TRUE)
-  mean_y <- mean(df$Y, na.rm = TRUE)
-  mean_z <- mean(df$Z, na.rm = TRUE)
-  
-  df$X_dynamic <- df$X - mean_x
-  df$Y_dynamic <- df$Y - mean_y
-  df$Z_dynamic <- df$Z - mean_z
-  
-  df$ODBA <- abs(df$X_dynamic) + abs(df$Y_dynamic) + abs(df$Z_dynamic)
-  df$VeDBA <- sqrt(df$X_dynamic^2 + df$Y_dynamic^2 + df$Z_dynamic^2)
-  
-  return(df)
-}
-
-# Aplicar a função de cálculo para cada indivíduo
-data <- data %>% group_by(ID) %>% do(calcular_vedba_odba(.))
-
-# Verificar as primeiras linhas dos dados
-head(data)
-
-# Criar gráficos de VeDBA para cada indivíduo com a variável Sexo
-plots_vedba <- list()
-for (individuo in individuos) {
-  # Filtrar dados por indivíduo
-  data_individuo <- subset(data, ID == individuo)
-  
-  # Gerar o gráfico de VeDBA ao longo do tempo para cada indivíduo
-ggplot(data_individuo, aes(x = Timestamp, y = VeDBA, color = SEX)) +
-    geom_line() +
-    labs(title = paste("VeDBA para o indivíduo", individuo),
-         x = "Tempo",
-         y = "VeDBA (m/s²)") +
-    theme_minimal()+
-  scale_x_datetime(date_breaks = "1 hour", date_labels = "%H:%M")
-
-  
-  # Adicionar o gráfico à lista
-  plots_vedba[[individuo]] <- p
-}
-
-# Combinar todos os gráficos de VeDBA em um único layout com patchwork
-combined_vedba_plots <- wrap_plots(plots_vedba)
-
-# Exibir todos os gráficos combinados de VeDBA
-print(combined_vedba_plots)
-
-# Salvar os dados com as novas colunas calculadas
-write.csv(data, "dados_com_vedba_odba_por_individuo_e_sexo.csv", row.names = FALSE)
 
 #------Média de profundidade de cada viagem de forrageio (por Foraging trip) ------ 
 
@@ -180,11 +126,6 @@ head(depth_stats_MS)
 # Visualizar as primeiras linhas das estatísticas calculadas para SP
 print("Statistics for São Pedro e São Paulo (SP):")
 head(depth_stats_SP)
-  )
-
-# Agora eu vou incluir esses resultados na planilha original (trip_metrics):
-
-trip_metrics
 
 # Carregar os dados de profundidade calculados e combinar as duas planilhas acima
 
@@ -195,20 +136,8 @@ depth_stats_SP <- depth_stats_SP %>%
 depth_stats_MS <- depth_stats_MS %>%
   mutate(Source = "MS")  # Adiciona a coluna "Source" com valor "MS"
 
-depth_stats_combined <- bind_rows(depth_stats_SP, depth_stats_MS)
-
-# Verificar se as colunas correspondem
-head(trip_metrics$tripID)  # TripID na planilha original
-head(depth_stats_combined$tripID)   # TripID na planilha de profundidade
-
-# Unir os dois conjuntos de dados com base na coluna TripID
-trip_metrics_updated <- trip_metrics %>%
-  left_join(depth_stats_combined, by = "tripID") %>% # Faz a união com base em TripID
-  select(-mean_depth)  # Exclui a coluna "mean_depth" que estava duplicada (só anotação)
-
-trip_metrics_updated
-
 # Salvar o resultado final em um novo arquivo CSV
-write.csv(trip_metrics_updated, "trip_metrics_with_depth.csv", row.names = FALSE)
+write.csv(depth_stats_SP, "depth_stats_SP_cf", row.names = FALSE)
+write.csv(depth_stats_MS, "depth_stats_MS_cf", row.names = FALSE)
 
-
+#Adicionei os resultados das estatísticas de mergulho de MS e SPSP manualmente na planilha trip_metrics e atualizei no GitHub
